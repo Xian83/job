c3a755bbfea13b3424a6f6409a083a5498fdb5b7
admin <admin@192.168.10.4>
2017-03-16 14:49:24 +0900

    시작페이지 - 검색

--- Job/src/controller/SearchController.java
+++ Job/src/controller/SearchController.java
@@ -1,24 +1,35 @@
 package controller;
 
-import java.util.Map;
+import java.io.IOException;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import model.SearchDao;
+
 @Controller
 @RequestMapping("/search")
 public class SearchController {
-
+	
+	@Autowired
+	SearchDao sdao;
+	
 	// 기업명으로 검색 
 	@RequestMapping("/name")
-	public ModelAndView searchByNameHandler(@RequestParam Map map){
+	public ModelAndView search1Handler(@RequestParam (name="search") String search) throws IOException{
 		
-				
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("t1");
 		mav.addObject("main", "search/list_form");
+		
+		System.out.println("param search == " + search );  
+
+			List list = sdao.search1(search);
+	
 		return mav;
 	}
 }
