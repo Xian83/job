eeb09f2d86528af8e062e6179973df5045c9cba4
admin <admin@192.168.10.10>
2017-03-24 15:44:52 +0900

    현정 : 상세 검색 페이지(Ajax 기능, 전체 선택 처리), 랭크 연결부 수정

--- Job/src/controller/RankController.java
+++ Job/src/controller/RankController.java
@@ -21,50 +21,19 @@ public class RankController {
 	// rank page main
 	@RequestMapping("/form")
 	public ModelAndView InitHandler() {
-		List list = rdao.avgsalary(50);
+		List list1 = rdao.avgsalary(50);
+		List list2 = rdao.rookiesalary(50);
+		List list3 = rdao.financeScore(50);
+		List list4 = rdao.employeeScore(50);
 		
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("t1");
 		mav.addObject("main", "rank/form");
-		mav.addObject("list", list);
+		mav.addObject("avg", list1);
+		mav.addObject("rookie", list2);
+		mav.addObject("finance", list3);
+		mav.addObject("employee", list4);
 				
-		return mav;
-	}
-
-	// ���� ���Կ���
-	@RequestMapping("/rookie")
-	public ModelAndView InitHandler2() {
-		List list = rdao.rookiesalary(50);
-		
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("t1");
-		mav.addObject("main", "rank/rookieSalary");
-		mav.addObject("list", list);
-		
-		return mav;
-	}
-
-	// 재무평가
-	@RequestMapping("/finance")
-	public ModelAndView InitHandler3() {
-		List list = rdao.financeScore(50);
-		
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("t1");
-		mav.addObject("main", "rank/financeScore");
-		mav.addObject("list", list);
-		return mav;
-	}
-
-	// 재직자 평가 점수
-	@RequestMapping("/employee")
-	public ModelAndView InitHandler4() {
-		List list = rdao.employeeScore(50);
-		
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("t1");
-		mav.addObject("main", "rank/employeeScore");
-		mav.addObject("list", list);
 		return mav;
 	}
 
