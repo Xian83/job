1e082c875087c7cad09c744c7fa8adc7b7fffbe1
NYC <NYC@NYC-PC>
2017-03-28 11:44:15 +0900

    현정 : 추천기업용 몽고DB 작업 중

--- Job/src/controller/MyPageController2.java
+++ Job/src/controller/MyPageController2.java
@@ -61,29 +61,32 @@ public class MyPageController2 {
 	@RequestMapping("/recommand")
 	public ModelAndView recommandHandler(HttpSession session) {
 		String email = (String) session.getAttribute("email");
-		HashMap data = mDao.getInfo(email); //(WKP_ADRS, STNDD_BIG_GB, SALARY_MIN, SALARY_MAX)
+		
+		// get member info data
+		//(WKP_ADRS, STNDD_BIG_GB, SALARY_MIN, SALARY_MAX)
+		HashMap data = mDao.getInfo(email); 
 		System.out.println("info : " + data.toString());// 확인용
 
 		//추천기업 정보 가져오기 
 		List reco = mypage.getRecommand(data); // CMPN_NM, Salary 정보 가져옴
 		// score db에서 cmpn_nm 기준으로 데이터 가져오기
-		List list = detail.getScoreData(reco);
+//		List list = detail.getScoreData(reco);
 		
-		for (Object map : list) {
-			if (((Map) map).get("LOGO") == null) {
-				((Map) map).put("LOGO", "http://image.jinhak.com/job/site/tmp02.gif");
-			}
-		}
-
+//		for (Object map : list) {
+//			if (((Map) map).get("LOGO") == null) {
+//				((Map) map).put("LOGO", "http://image.jinhak.com/job/site/tmp02.gif");
+//			}
+//		}
+//
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("tt");
 		mav.addObject("main", "/my/recommand");
-		mav.addObject("list", list);
-		
-		// 사진 추가
-		String picURL = mydao.getLastetImageURL(email);
-		if (picURL == null || picURL.equals("null"))
-			picURL = "/picture/default.jpg";
+//		mav.addObject("list", list);
+//		
+//		// 사진 추가
+//		String picURL = mydao.getLastetImageURL(email);
+//		if (picURL == null || picURL.equals("null"))
+//			picURL = "/picture/default.jpg";
 		
 		return mav;
 	}
