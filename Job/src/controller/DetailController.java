477e757b9cf47fa4ba2f85e6aa9c3be76412a548
JM <JM@192.168.35.32>
2017-04-01 15:59:26 +0900

    종민 컨택트

--- Job/src/controller/DetailController.java
+++ Job/src/controller/DetailController.java
@@ -52,7 +52,7 @@ public class DetailController {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("ttt");
 		mav.addObject("main", "company/detail_form");
-		HashMap totalvisit = ddao.inqurity(companyname);// 회사 조회수 증가 and 총 조회수
+		HashMap totalvisit = ddao.inqurity(companyname);// �쉶�궗 議고쉶�닔 利앷� and 珥� 議고쉶�닔
 
 		// get data by Company name
 		HashMap scorelist = ddao.score(companyname);
@@ -60,13 +60,13 @@ public class DetailController {
 		List reviewList = ddao.review(companyname);
 		String chartURL = makeChart(ddao.getScore02(companyname));// graph
 
-		// member check & 관심기업 여부 확인
+		// member check & 愿��떖湲곗뾽 �뿬遺� �솗�씤
 		String email = (String) session.getAttribute("email");
 		if (email == null) {
 			email = "visitant";
 		} else {
 			int a = ddao.checkScrape(companyname, email);
-			mav.addObject("scrape", a); // 0 : 아님, 1: 관심기업
+			mav.addObject("scrape", a); // 0 : �븘�떂, 1: 愿��떖湲곗뾽
 		}
 
 		// increase visit count to DB
@@ -75,13 +75,13 @@ public class DetailController {
 		// get same industry company list (finance score desc)
 		String div = (String) scorelist.get("DIVISION");
 		List samelist = ddao.same(div);
-		// System.out.println("산업군 : " + div); //확인용
+		// System.out.println("�궛�뾽援� : " + div); //�솗�씤�슜
 
 		// salary info
 		HashMap industry = sDao.getSalary(div); // same industry
 		HashMap allCompany = sDao.getSalary("all"); // all company
 		// System.out.println(industry.get("AVG") + " vs " +
-		// industry.get("ROOKIE")); // 확인용
+		// industry.get("ROOKIE")); // �솗�씤�슜
 
 		String CompID = search.getCompID(companyname);
 		// career catch site data
@@ -99,14 +99,14 @@ public class DetailController {
 		mav.addObject("info02", info02);// HashMap (summary, address, system,
 										// culture)
 		mav.addObject("json", google.map((String) info02.get("address")));
-		mav.addObject("chartURL", chartURL); // 방사형 그래프 주소
-		mav.addObject("rate", rate); // 상세페이지를 클릭한 남녀비율 hashmap(man, woman)
-		mav.addObject("total", totalvisit); // 전체 조회수 hashmap(sum)
+		mav.addObject("chartURL", chartURL); // 諛⑹궗�삎 洹몃옒�봽 二쇱냼
+		mav.addObject("rate", rate); // �긽�꽭�럹�씠吏�瑜� �겢由��븳 �궓��鍮꾩쑉 hashmap(man, woman)
+		mav.addObject("total", totalvisit); // �쟾泥� 議고쉶�닔 hashmap(sum)
 
-		// 쿠키생성
-		String[] arr = origin.split("#"); // 봤던 쿠키 목록
-		// 이 배열에 companyname 이 값이 있냐 없냐..
-		// 배열에 없는 회사명일때만 origin+"#"+companyname 이걸로 쿠키를 전송을 시켜
+		// 荑좏궎�깮�꽦
+		String[] arr = origin.split("#"); // 遊ㅻ뜕 荑좏궎 紐⑸줉
+		// �씠 諛곗뿴�뿉 companyname �씠 媛믪씠 �엳�깘 �뾾�깘..
+		// 諛곗뿴�뿉 �뾾�뒗 �쉶�궗紐낆씪�븣留� origin+"#"+companyname �씠嫄몃줈 荑좏궎瑜� �쟾�넚�쓣 �떆耳�
 		// System.out.println("origin="+origin);
 		boolean rst = false;
 		for (String cc : arr) {
@@ -115,6 +115,7 @@ public class DetailController {
 				break;
 			}
 		}
+
 		if (rst == false) {
 			origin = companyname + "#" + origin;
 			Cookie c = new Cookie("cmpn_nm", origin);
@@ -123,7 +124,7 @@ public class DetailController {
 			// System.out.println("coo="+origin);
 		}
 
-		// 쿠키처리
+		// 荑좏궎泥섎━
 		List<String> cookielist = new ArrayList<>();
 		if (!origin.equals("")) {
 			String[] ar = origin.split("#");
@@ -141,7 +142,7 @@ public class DetailController {
 		mav.addObject("clist", cookielist);
 		mav.addObject("csize", csize);
 
-		// 로그인했을때 추천리스트추가
+		// 濡쒓렇�씤�뻽�쓣�븣 異붿쿇由ъ뒪�듃異붽�
 		String auth = (String) session.getAttribute("auth");
 
 		if (auth != null && auth.equals("yes")) {
@@ -152,10 +153,10 @@ public class DetailController {
 			List list_r = mypage.getRecommand02(reco); // get data from score &
 														// salary table
 
-			mav.addObject("member", data); // 관심지역(AREA),산업군(STNDD_BIG_GB),
-											// 연봉min/max
+			mav.addObject("member", data); // 愿��떖吏��뿭(AREA),�궛�뾽援�(STNDD_BIG_GB),
+											// �뿰遊뎜in/max
 			mav.addObject("list_r", list_r);
-			System.out.println("추천 = " + list_r);
+			System.out.println("異붿쿇 = " + list_r);
 		}
 
 		return mav;
@@ -180,22 +181,22 @@ public class DetailController {
 		img += data1.get("LABEL04") + "," + data1.get("LABEL05") + "," + data1.get("LABEL06") + ",";
 		img += data1.get("LABEL07") + "," + data1.get("LABEL08") + "," + data1.get("LABEL09") + ","
 				+ data1.get("LABEL01");
-		// img += "&chco=#FF0000"; // 선 색깔
+		// img += "&chco=#FF0000"; // �꽑 �깋源�
 		img += "&chls=2.0,4.0,0.0&chxt=x";
-		img += "&chxl=0:|규모·형태|안정성|성장성|수익성|조직문화·분위기|급여·복리후생|근무시간·휴가|성장·경력|경영진·경영";
+		img += "&chxl=0:|洹쒕え쨌�삎�깭|�븞�젙�꽦|�꽦�옣�꽦|�닔�씡�꽦|議곗쭅臾명솕쨌遺꾩쐞湲�|湲됱뿬쨌蹂듬━�썑�깮|洹쇰Т�떆媛꽷룻쑕媛�|�꽦�옣쨌寃쎈젰|寃쎌쁺吏꽷룰꼍�쁺";
 		img += "&chxr=0,0.0,360.0";
 		return img;
 	}
 
 	@RequestMapping("/basic_info")
-	public ModelAndView basicInfoHandler(@RequestParam(name = "cmpn_nm", defaultValue = "컴투스") String CName,
+	public ModelAndView basicInfoHandler(@RequestParam(name = "cmpn_nm", defaultValue = "而댄닾�뒪") String CName,
 			HttpSession session) {
 		
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("t1");
 		mav.addObject("main", "/company/basic_info");
 
-		HashMap totalvisit = ddao.inqurity(CName);// 회사 조회수 증가 and 총 조회수
+		HashMap totalvisit = ddao.inqurity(CName);// �쉶�궗 議고쉶�닔 利앷� and 珥� 議고쉶�닔
 
 		// get data by Company name
 		HashMap scorelist = ddao.score(CName);
@@ -203,13 +204,13 @@ public class DetailController {
 		List reviewList = ddao.review(CName);
 		String chartURL = makeChart(ddao.getScore02(CName));// graph
 
-		// member check & 관심기업 여부 확인
+		// member check & 愿��떖湲곗뾽 �뿬遺� �솗�씤
 		String email = (String) session.getAttribute("email");
 		if (email == null) {
 			email = "visitant";
 		} else {
 			int a = ddao.checkScrape(CName, email);
-			mav.addObject("scrape", a); // 0 : 아님, 1: 관심기업
+			mav.addObject("scrape", a); // 0 : �븘�떂, 1: 愿��떖湲곗뾽
 		}
 
 		// increase visit count to DB
@@ -218,20 +219,20 @@ public class DetailController {
 		// get same industry company list (finance score desc)
 		String div = (String) scorelist.get("DIVISION");
 		List samelist = ddao.same(div);
-		// System.out.println("산업군 : " + div); //확인용
+		// System.out.println("�궛�뾽援� : " + div); //�솗�씤�슜
 
 		// salary info
 		HashMap industry = sDao.getSalary(div); // same industry
 		HashMap allCompany = sDao.getSalary("all"); // all company
 		// System.out.println(industry.get("AVG") + " vs " +
-		// industry.get("ROOKIE")); // 확인용
+		// industry.get("ROOKIE")); // �솗�씤�슜
 
 		String CompID = search.getCompID(CName);
 		// career catch site data
 		HashMap<String, List> info01 = ddao.getInfo01(CompID);
 		HashMap<String, Object> info02 = ddao.getInfo02(CompID);
 
-		// 1주일간 조회수 그래프
+		// 1二쇱씪媛� 議고쉶�닔 洹몃옒�봽
 		List<HashMap> visitgraph = mypage.visitgraph(CName);
 		ArrayList vList = new ArrayList();
 		for (HashMap map : visitgraph) {
@@ -248,10 +249,10 @@ public class DetailController {
 		mav.addObject("info01", info01);// HashMap<List>(rank8, employee, scale)
 		mav.addObject("info02", info02);// HashMap (summary, address, system, culture)
 		mav.addObject("json", google.map((String) info02.get("address")));
-		mav.addObject("chartURL", chartURL); // 방사형 그래프 주소
-		mav.addObject("rate", rate); // 상세페이지를 클릭한 남녀비율 hashmap(man, woman)
-		mav.addObject("total", totalvisit); // 전체 조회수 hashmap(sum)
-		mav.addObject("vList", vList); // 최근 1주일간 조회수 변화 그래프용
+		mav.addObject("chartURL", chartURL); // 諛⑹궗�삎 洹몃옒�봽 二쇱냼
+		mav.addObject("rate", rate); // �긽�꽭�럹�씠吏�瑜� �겢由��븳 �궓��鍮꾩쑉 hashmap(man, woman)
+		mav.addObject("total", totalvisit); // �쟾泥� 議고쉶�닔 hashmap(sum)
+		mav.addObject("vList", vList); // 理쒓렐 1二쇱씪媛� 議고쉶�닔 蹂��솕 洹몃옒�봽�슜
 		
 		return mav;
 	}
